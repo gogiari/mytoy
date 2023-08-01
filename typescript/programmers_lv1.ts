1b0fb127a76efc2cc571ce31276578b19c794fed
gogiari <gomul82@gmail.com>
2023-08-02 05:42:05 +0900

    프로그래머스 lv2 ?카카오 오픈채팅

--- typescript/programmers_lv1.ts
+++ typescript/programmers_lv1.ts
@@ -1,6 +1,5 @@
-
-
-const line: string = "=============================================================================";
+const line: string =
+  "=============================================================================";
 // ================================ 달리기 경주 =====================================================
 // https://school.programmers.co.kr/learn/courses/30/lessons/178871
 /*
@@ -27,73 +26,114 @@ function runningRace(players: string[], callings: string[]){
 }
 */
 function runningRace(players: string[], callings: string[]) {
-    let idx;
-    let player1;
-    let player2;
-    const map: { [key: string]: number } = {};
+  let idx;
+  let player1;
+  let player2;
+  const map: { [key: string]: number } = {};
 
+  players.forEach((name, index) => (map[name] = index));
+  console.log(map["mine"]);
 
-    players.forEach((name,index)=>map[name]=index)
-    console.log(map["mine"]);
+  for (var call of callings) {
+    idx = map[call];
+    player1 = players[idx];
+    player2 = players[idx - 1];
+    map[call] -= 1;
+    map[player2] += 1;
+    players[idx] = player2;
+    players[idx - 1] = player1;
+  }
 
-    for(var call of callings){
-        idx = map[call];
-        player1 = players[idx];
-        player2 = players[idx-1];
-        map[call] -= 1;
-        map[player2] += 1;
-        players[idx] = player2;
-        players[idx-1] = player1;
-    }
-
-    console.log(players);
-    console.log(line)
+  console.log(players);
+  console.log(line);
 }
 
-
-runningRace( ["mumu", "soe", "poe", "kai", "mine"]  , ["kai", "kai", "mine", "mine"] );
-
+runningRace(
+  ["mumu", "soe", "poe", "kai", "mine"],
+  ["kai", "kai", "mine", "mine"]
+);
 
 // =================================== 추억점수 ================================================
 // https://school.programmers.co.kr/learn/courses/30/lessons/176963
 
-function oldScore(name: string[], yearning: number[], photo: string[][]){
-    const scoreMap: { [key: string]: number } = {};
-    var answer: number[] = new Array(photo.length).fill(0);
-    name.forEach((n, index) => {
-        scoreMap[n] = yearning[index];
+function oldScore(name: string[], yearning: number[], photo: string[][]) {
+  const scoreMap: { [key: string]: number } = {};
+  var answer: number[] = new Array(photo.length).fill(0);
+  name.forEach((n, index) => {
+    scoreMap[n] = yearning[index];
+  });
+  console.log(scoreMap);
+  photo.forEach((row, idx) => {
+    row.forEach((col) => {
+      // console.log("<<<<<<<<<<<<<<배열: "+idx);
+      // console.log(col);
+      // console.log(scoreMap[col]);
+      if (scoreMap.hasOwnProperty(col)) {
+        answer[idx] += scoreMap[col];
+      }
     });
-    console.log(scoreMap);
-    photo.forEach((row, idx) => {
-        row.forEach(col => {
-            // console.log("<<<<<<<<<<<<<<배열: "+idx);
-            // console.log(col);
-            // console.log(scoreMap[col]);
-            if (scoreMap.hasOwnProperty(col)) {
-                answer[idx] += scoreMap[col];
-            }
-        });
+  });
+  console.log(answer);
+  console.log(line);
+  return answer;
+}
+oldScore(
+  ["may", "kein", "kain", "radi"],
+  [5, 10, 1, 3],
+  [
+    ["may", "kein", "kain", "radi"],
+    ["may", "kein", "brin", "deny"],
+    ["kon", "kain", "may", "coni"],
+  ]
+);
+
+function oldScore2(name: string[], yearning: number[], photo: string[][]) {
+  console.log("점수2");
+  // console.log( name.indexOf("may") );
+  // console.log(photo.map((v) => v.reduce((a, c) => a)));
+  console.log(
+    photo.map((v) =>
+      v.reduce((a, c) => (a += yearning[name.indexOf(c)] ?? 0), 0)
+    )
+  );
+  return photo.map((v) =>
+    v.reduce((a, c) => (a += yearning[name.indexOf(c)] ?? 0), 0)
+  );
+}
+oldScore2(
+  ["may", "kein", "kain", "radi"],
+  [5, 10, 1, 3],
+  [
+    ["may", "kein", "kain", "radi"],
+    ["may", "kein", "brin", "deny"],
+    ["kon", "kain", "may", "coni"],
+  ]
+);
+
+function kakaochat(record: string[]) {
+    const answer: string[] = [];
+    const map: { [key: string]: string } = {};
+    record.forEach(e => {
+        const[action, uid, name] = e.split(" ");
+        if( action === "Enter" || action === "Change" ){
+            map[uid] = name;
+        }
     });
+
+    record.forEach(e => {
+        const[action, uid, name] = e.split(" ");
+        action ==="Enter" ? answer.push(`${map[uid]}님이 들어왔습니다.`) : action === "Leave" ? answer.push(`${map[uid]}님이 나갔습니다.`) : null ;
+    });
+
     console.log(answer);
-    console.log(line);
     return answer;
 }
-oldScore( ["may", "kein", "kain", "radi"],
-        [5, 10, 1, 3],
-        [["may", "kein", "kain", "radi"],["may", "kein", "brin", "deny"], ["kon", "kain", "may", "coni"]]
-)
-
-function oldScore2(name: string[], yearning: number[], photo: string[][]){
-    console.log("점수2");
-    // console.log( name.indexOf("may") );
-    // console.log(photo.map((v) => v.reduce((a, c) => a)));
-    console.log(photo.map((v)=> v.reduce((a, c)=> a += yearning[name.indexOf(c)] ?? 0, 0)));
-    return photo.map((v)=> v.reduce((a, c)=> a += yearning[name.indexOf(c)]??  0, 0));
-}
-oldScore2( ["may", "kein", "kain", "radi"],
-        [5, 10, 1, 3],
-        [["may", "kein", "kain", "radi"],["may", "kein", "brin", "deny"], ["kon", "kain", "may", "coni"]]
-)
 
 
-
+kakaochat([
+  "Enter uid1234 Muzi",
+  "Enter uid4567 Prodo",
+  "Leave uid1234",
+  "Enter uid1234 Prodo",
+  "Change uid4567 Ryan",
+]);
